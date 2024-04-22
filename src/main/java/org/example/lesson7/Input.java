ee631155cc1f89f773f427547e249155021e3f6e
danii <daniilkaniga@gmail.com>
2024-04-22 21:14:28 +0300

    lesson 6 If 15.04

---
+++ src/main/java/org/example/lesson7/Input.java
@@ -0,0 +1,17 @@
+package org.example.lesson7;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число");
+        int n = scanner.nextInt();
+
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Введите строку");
+        String s = scanner1.nextLine();
+
+
+    }
+}
---
+++ src/main/java/org/example/lesson8/DoWhile.java
@@ -0,0 +1,17 @@
+package org.example.lesson8;
+
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int value;
+        do {
+            System.out.println("Введите 5");
+            value = scanner.nextInt();
+        }
+        while (value!=5);
+
+        System.out.println("Вы ввели " + value);
+    }
+}
