ed0fe4c1c2897bf5dd8eae739d32f955f8e01600
danii <daniilkaniga@gmail.com>
2024-04-24 22:37:25 +0300

    Уроки за 24.04

--- src/main/java/org/example/lesson11/Arrays.java
+++ src/main/java/org/example/lesson11/Arrays.java
@@ -1,4 +1,18 @@
 package org.example.lesson11;
 
 public class Arrays {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+
+            arr[i] = i*3;
+
+        }
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.println(arr[i]);
+        }
+    }
+
+
+
 }
